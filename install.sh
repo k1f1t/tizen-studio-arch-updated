ce57c3410bedcb29768f1975ee75c3e3fd7e0c5b
k1f1t <106300709+k1f1t@users.noreply.github.com>
2025-05-07 17:41:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+./dpkg
+./fake-dpkg.sh
+if test -f tizen_studio.bin; then
+    echo "File exists. Continuing with downloaded version. If you want to update just delete tizen_studio.bin file"
+else
+    wget -O tizen_studio.bin https://download.tizen.org/sdk/Installer/tizen-studio_6.1/web-ide_Tizen_Studio_6.1_ubuntu-64.bin
+    # official link https://download.tizen.org/sdk/Installer/tizen-studio_4.5/web-ide_Tizen_Studio_4.5_ubuntu-64.bin
+    # For US CDN
+    # wget -O tizen_studio.bin http://usa.sdk-dl.tizen.org/web-ide_Tizen_Studio_4.5_usa_ubuntu-64.bin
+fi
 
-wget -O tizen_studio.bin https://download.tizen.org/sdk/Installer/tizen-studio_4.5/web-ide_Tizen_Studio_4.5_ubuntu-64.bin
-# For US CDN
-# wget -O tizen_studio.bin http://usa.sdk-dl.tizen.org/web-ide_Tizen_Studio_4.5_usa_ubuntu-64.bin
 chmod +x tizen_studio.bin
 PATH=$(pwd):$PATH ./tizen_studio.bin
